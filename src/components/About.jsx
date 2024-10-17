f986453f9cfb3f48b6ac2c53a0543cab3e87c8f3
kiinshuk <red2k2green@gmail.com>
2024-10-18 02:01:54 +0530

    exp added resume updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,17 @@ import '../App.css';
 
 const experiences = [
   {
+    title: "Front End Developer - Yugox Pvt Ltd",
+    description: `
+      Focused on front-end development using <span class="technology">React.js</span>. 
+      Tested APIs for integrating <span class="technology">3CX</span> with CRM systems using <span class="technology">Postman</span>.
+      Designed and implemented user interfaces for web applications, enhancing their visual appeal and functionality.`
+  },
+  {
     title: "Software Engineer Intern - Katha Infocom Pvt Ltd",
     description: `Focused on front-end development using <span class="technology">React.js</span> and <span class="technology">Tailwind CSS</span> to enhance web applications.`
   }
+  
 ];
 
 const About = () => {
@@ -34,9 +42,10 @@ const About = () => {
               {activeIndex === index ? '▲' : '▼'}
             </span>
           </div>
+
           <div className="experience-description">
             <div className='exp'>
-              <p className='period'>Jan '24 - Jun '24</p>
+              {index === 0 ? <p className='period'>Aug '24 - Current</p> : <p className='period'>Jan '24 - Jun '24</p>}
             </div>
             <p dangerouslySetInnerHTML={{ __html: experience.description }}></p>
           </div>
